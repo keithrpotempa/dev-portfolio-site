608335b60605c05b6271be73c2bba115115789fa
Keith Potempa <keithrpotempa@gmail.com>
2020-08-20 13:39:41 -0500

    commented out the use of codewars fetch

--- assets/js/htmlBuilders/learningProfileBuilder.js
+++ assets/js/htmlBuilders/learningProfileBuilder.js
@@ -3,16 +3,16 @@ import objectSorter from "../helpers/objectSorter.js";
 
 const treeHouseProfile = apiManager.getTreeHouseProfile();
 const codeWarsProfile = apiManager.getCodeWarsProfile();
-Promise.all([treeHouseProfile, codeWarsProfile])
-  .then(response => {
-    learningProfile("Treehouse", response[0])
-    console.log("CodeWars", response[1])
-  })
-// Promise.all([treeHouseProfile])
-  // .then(response => response.forEach(profile => {
-    // learningProfile("Treehouse", profile)
-    // console.log("CodeWars", )
-  // })) 
+// Promise.all([treeHouseProfile, codeWarsProfile])
+//   .then(response => {
+//     learningProfile("Treehouse", response[0])
+//     console.log("CodeWars", response[1])
+//   })
+Promise.all([treeHouseProfile])
+  .then(response => response.forEach(profile => {
+    learningProfile("Treehouse", profile)
+    console.log("CodeWars", )
+  })) 
 
 const profileRowBuilder = (allRows) => {
   let html = "";
